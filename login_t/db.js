750521a1c5adcb5c0323fdb72c97d02166eee1c1
Brian Mai <0hrs3g@gmail.com>
2023-08-18 14:46:30 -0700

    exp + misc.

--- login_t/db.js
+++ login_t/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
+const app = express();
 
 // Connection URL
 const mguser = process.env.MG_USERNAME;
@@ -11,33 +12,15 @@ const mgpass = process.env.MG_PASSWORD;
 // console.log(mguser, mgpass); 
 
 const uri = `mongodb+srv://${mguser}:${mgpass}@cluster0.uwijlbo.mongodb.net/?retryWrites=true&w=majority`; // MongoDB server URL
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true } );
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true } )
+  .then((result) => {
 
-
-
-
-
+    console.log('connected to db.')
+    app.listen(3000);
+    
+  })
+  .catch((err) => console.log(err));
 
 
 mongoose.disconnect()
 
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     await client.connect(); // Connect the client to the server	(optional starting in v4.7)
-//     await client.db("admin").command({ ping: 1 });  // Send a ping to confirm a successful connection
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
